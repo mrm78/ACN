b6c247d7c33e666d4b31a070b34d9a7a56dd252e
mohamad <mr.mirshamsi.78@gmail.com>
2021-05-30 16:17:26 +0430

    fix asgi issue

--- ACN_backend_service/ACN2/asgi.py
+++ ACN_backend_service/ACN2/asgi.py
@@ -1,10 +1,12 @@
-import os
+import os, django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ACN2.settings")
+django.setup()
+
 from chats.middleware.CustomAuth import TokenAuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chats.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ACN2.settings")
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
